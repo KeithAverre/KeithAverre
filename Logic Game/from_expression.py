937174a6306ba76f7aa03e31c6fa7da4e7986cea
KeithAverre <kmaverre@alaska.edu>
2021-12-22 00:38:12 -0900

    Future plans with from_expressions

--- Logic Game/from_expression.py
+++ Logic Game/from_expression.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+"""
+This file is to hold all expression handling to avoid clutter within other files where it would be out of place.
+
+-Format checking; complete parenthesis, no unusual characters, 
+-logic checking and reducing
+-Keep track of all unique alphabet characters
+-Give to logic.py to compute a Logic object.
+
+"""
 
 def startComputing():
     raise Exception("Function is not functional yet. From expression")
